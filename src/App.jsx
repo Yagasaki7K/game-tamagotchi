7b55da68f6ee5fd328d556c1c5abae1f5654374a
Anderson Marlon <anderson18.marlon@gmail.com>
2024-05-29 13:09:19 -0300

    Added: Study Method - 29-05 13:09

--- src/App.jsx
+++ src/App.jsx
@@ -1,116 +1,213 @@
 import React, { useState, useEffect } from "react";
 import styled from 'styled-components';
+import { toast } from 'sonner';
 
 export default function Tamagotchi() {
-    const [hunger, setHunger] = useState(50);
-    const [happiness, setHappiness] = useState(50);
-    const [energy, setEnergy] = useState(50);
+    const [hunger, setHunger] = useState(() => parseInt(localStorage.getItem('hunger')) || 50);
+    const [happiness, setHappiness] = useState(() => parseInt(localStorage.getItem('happiness')) || 50);
+    const [energy, setEnergy] = useState(() => parseInt(localStorage.getItem('energy')) || 50);
+    const [study, setStudy] = useState(() => parseInt(localStorage.getItem('study')) || 20);
+    const [noteStudy, setNoteStudy] = useState('F');
+    const [noteColor, setNoteColor] = useState('#e10065');
 
-    // Define um temporizador que decrementa os valores de fome,
-    // felicidade e energia a cada 5 segundos
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setHunger((prevHunger) => prevHunger - 5);
-            setHappiness((prevHappiness) => prevHappiness - 3);
-            setEnergy((prevEnergy) => prevEnergy - 2);
-        }, 5000);
+            setHunger((prevHunger) => Math.max(prevHunger - 5, 0));
+            setHappiness((prevHappiness) => Math.max(prevHappiness - 3, 0));
+            setEnergy((prevEnergy) => Math.max(prevEnergy - 2, 0));
+            setStudy((prevStudy) => Math.max(prevStudy - 1, 0));
+        }, 10000);
 
-        // Limpa o temporizador quando o componente é desmontado
         return () => clearInterval(intervalId);
     }, []);
 
-    // Verifica o estado do Tamagotchi e atualiza a animação ou som de acordo
     useEffect(() => {
+        localStorage.setItem('hunger', hunger);
+        localStorage.setItem('happiness', happiness);
+        localStorage.setItem('energy', energy);
+        localStorage.setItem('study', study)
+    }, [hunger, happiness, energy]);
+
+    useEffect(() => {
+        if (energy <= 0 && happiness <= 20 && hunger <= 20) {
+            toast.error('Seu tamagotchi morreu!');
+        }
+
         if (energy <= 0) {
-            // Tamagotchi está cansado e precisa dormir
-            // Exibe animação de Tamagotchi dormindo
+            toast.warning('Tamagotchi está cansado e precisa dormir!');
         }
+
         if (happiness <= 20) {
-            // Tamagotchi está triste e precisa de atenção
-            // Toca som de felicidade
+            toast.warning('Tamagotchi está triste e precisa de atenção!');
         }
-        if (hunger >= 80) {
-            // Tamagotchi está com fome e precisa comer
-            // Exibe animação de Tamagotchi comendo
+
+        if (hunger <= 20) {
+            toast.warning('Tamagotchi está com fome e precisa de comida!');
         }
+
+        if (hunger === 70) {
+            toast.warning('Tamagotchi está satisfeito, ele pode passar mal');
+        }
+
+        if (hunger >= 90) {
+            toast.error('Tamagotchi passou mal!');
+            setHappiness((prevHappiness) => Math.max(prevHappiness - 30, 0));
+            setHunger((prevHunger) => Math.max(prevHunger - 30, 0));
+            setStudy((prevStudy) => Math.max(prevStudy - 10, 0));
+            setEnergy((prevEnergy) => Math.max(prevEnergy - 20, 0));
+        }
+
+        switch (true) {
+            case (study >= 95):
+                setNoteStudy("A+");
+                setNoteColor("#8BC34A");
+                break;
+            case (study >= 90):
+                setNoteStudy("A")
+                setNoteColor("#8BC34A");
+                break;
+            case (study >= 85):
+                setNoteStudy("B+");
+                setNoteColor("#8BC34A");
+                break;
+            case (study >= 80):
+                setNoteStudy("B")
+                setNoteColor("#8BC34A");
+                break;
+            case (study >= 75):
+                setNoteStudy("C+");
+                setNoteColor("#777774");
+                break;
+            case (study >= 70):
+                setNoteStudy("C")
+                setNoteColor("#777774");
+                break;
+            case (study >= 65):
+                setNoteStudy("D+");
+                setNoteColor("#777774");
+                break;
+            case (study >= 50):
+                setNoteStudy("D")
+                setNoteColor("#777774");
+                break;
+            case (study >= 45):
+                setNoteStudy("E+");
+                setNoteColor("#777774");
+                break;
+            case (study >= 40):
+                setNoteStudy("E")
+                setNoteColor("#e10065");
+                break;
+            case (study >= 35):
+                setNoteStudy("F+");
+                setNoteColor("#e10065");
+                break;
+            default:
+                setNoteStudy("F")
+                setNoteColor("#e10065");
+                break;
+        }
+
+
     }, [energy, happiness, hunger]);
 
-    // Funções para interagir com o Tamagotchi
     function feed() {
-        setHunger((prevHunger) => prevHunger + 10);
+        setHunger((prevHunger) => Math.min(prevHunger + 10, 100));
+        toast.success('Alimentado!');
     }
 
     function play() {
-        setHappiness((prevHappiness) => prevHappiness + 5);
-        setEnergy((prevEnergy) => prevEnergy - 5);
+        setHappiness((prevHappiness) => Math.min(prevHappiness + 5, 100));
+        setEnergy((prevEnergy) => Math.max(prevEnergy - 5, 0));
+        toast.success('Ele se divertiu!');
     }
 
     function sleep() {
-        setEnergy((prevEnergy) => prevEnergy + 20);
+        setEnergy((prevEnergy) => Math.min(prevEnergy + 20, 100));
+        toast.success('Dormindo ...!');
+    }
+
+    function studying() {
+        setStudy((prevStudy) => Math.min(prevStudy + 10, 100));
+        toast.success('Estudando ...!');
     }
 
     return (
-        <div>
-            <p>Hunger: {hunger}</p>
-            <p>Happiness: {happiness}</p>
-            <p>Energy: {energy}</p>
-            <FeedButton onClick={feed}>Feed</FeedButton>
-            <PlayButton onClick={play}>Play</PlayButton>
-            <SleepButton onClick={sleep}>Sleep</SleepButton>
-        </div>
+        <Container>
+            <h1>Tamagotchi</h1>
+            <img src="0.gif" />
+
+            <div className="flex">
+                <p>Estudo: <StudyState color={noteColor}>{noteStudy}</StudyState></p>
+                {/* <p>Idade: {age}</p>
+                <p>Sexo: {gender}</p> */}
+            </div>
+            <div className="flex">
+                <p>Fome: {hunger}</p>
+                <p>Felicidade: {happiness}</p>
+                <p>Energia: {energy}</p>
+                <p>Estudo: {study}</p>
+            </div>
+
+            <div className="flex">
+                <FeedButton onClick={feed}>Alimentar</FeedButton>
+                <PlayButton onClick={play}>Brincar</PlayButton>
+                <StudyButton onClick={studying}>Estudar</StudyButton>
+                <SleepButton onClick={sleep}>Dormir</SleepButton>
+            </div>
+        </Container>
     );
 }
 
-const FeedButton = styled.button`
-  background-color: #8BC34A;
-  border: none;
-  border-radius: 5px;
-  color: #FFFFFF;
-  font-size: 16px;
-  margin: 10px;
-  padding: 10px;
-  text-align: center;
-  text-decoration: none;
-  width: 100px;
-  
-  &:hover {
-    cursor: pointer;
-    opacity: 0.8;
-  }
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 20px;
+
+    .flex {
+        display: flex;
+
+        p {
+            margin: 0 10px;
+        }
+    }   
 `;
 
-const PlayButton = styled.button`
-  background-color: #2196F3;
-  border: none;
-  border-radius: 5px;
-  color: #FFFFFF;
-  font-size: 16px;
-  margin: 10px;
-  padding: 10px;
-  text-align: center;
-  text-decoration: none;
-  width: 100px;
-  
-  &:hover {
-    cursor: pointer;
-    opacity: 0.8;
-  }
+const Button = styled.button`
+    background: #8BC34A;
+    border: none;
+    border-radius: 5px;
+    color: #FFFFFF;
+    font-size: 16px;
+    margin: 10px;
+    padding: 10px;
+    text-align: center;
+    width: 100px;
+
+    &:hover {
+        cursor: pointer;
+        opacity: 0.8;
+    }
+    `;
+
+const FeedButton = styled(Button)`
+        background: #8BC34A;
+    `;
+
+const PlayButton = styled(Button)`
+        background: #2196F3;
+    `;
+
+const SleepButton = styled(Button)`
+        background: #9E9E9E;
+    `;
+
+const StudyButton = styled(Button)`
+        background: #FF9800;
 `;
 
-const SleepButton = styled.button`
-  background-color: #9E9E9E;
-  border: none;
-  border-radius: 5px;
-  color: #FFFFFF;
-  font-size: 16px;
-  margin: 10px;
-  padding: 10px;
-  text-align: center;
-  text-decoration: none;
-  width: 100px;
-  
-  &:hover {
-    cursor: pointer;
-    opacity: 0.8;
-  }
-`;
+const StudyState = styled.span`
+    font-weight: bold;
+    color: ${props => props.color}
+`
